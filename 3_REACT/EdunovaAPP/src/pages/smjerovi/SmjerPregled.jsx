0bff039ef104ca6124348939ff04516d99a18d2b
hrki1971 <h.mitric@gmail.com>
2026-03-19 17:38:39 +0100

    Formatiranjedatuma

---
+++ 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react"
+import SmjerService from "../../services/smjerovi/SmjerService"
+import { Table } from "react-bootstrap"
+import { NumericFormat } from "react-number-format"
+import { GrValidate } from "react-icons/gr"
+import FormatDatuma from "../../components/FormatDatuma"
+
+export default function SmjerPregled(){
+
+
+    const [smjerovi, setSmjerovi] = useState([])
+
+
+    useEffect(()=>{
+        ucitajSmjerove()
+    },[])
+
+    async function ucitajSmjerove() {
+        await SmjerService.get().then((odgovor)=>{
+            setSmjerovi(odgovor.data)
+        })
+    }
+
+
+    return(
+        <>
+    <Table>
+        <thead>
+            <tr>
+                <th>Naziv</th>
+                <th>Trajanje</th>
+                <th>Cijena</th>
+                <th>Datum pokretanja</th>
+                <th>Aktivan</th>
+                <th>Akcija</th>
+            </tr>
+        </thead>
+        <tbody>
+            {smjerovi && smjerovi.map((smjer)=>(
+                <tr>
+                    <td>{smjer.naziv}</td>
+                    <td>{smjer.trajanje}</td>
+                    <td>
+                        <NumericFormat
+                        value={smjer.cijena}
+                        displayType={'text'}
+                        thousandSeparator='.'
+                        decimalSeparator=','
+                        suffix={' €'}
+                        decimalScale={2}
+                        fixedDecimalScale
+                
+                        
+                        />
+                    </td>
+                    <td>
+                        <FormatDatuma datum={smjer.datumPokretanja} />
+                    </td>
+                    <td>
+                        <GrValidate 
+                        size={25}
+                        color={smjer.aktivan ? 'grean' : 'red'}
+                        />
+                    </td>
+                    <td></td>
+                </tr>
+            ))}
+        </tbody>
+    </Table>
+        </>
+    )
+}
